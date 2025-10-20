0d88d0a0e3cde72690202dc45887a3f49de082c5
ONKAR WAGH <onkar.s.wagh@gamil.com>
2025-10-20 19:26:45 +0530

    Initial commit

---
+++ trade-reqs/src/main/java/com/example/client/trade_reqs/TradeReqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.client.trade_reqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeReqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeReqsApplication.class, args);
+	}
+
+}
---
+++ trade-reqs/src/test/java/com/example/client/trade_reqs/TradeReqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client.trade_reqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
