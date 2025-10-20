afbbdc8c8f6965191b4c9d23048508f93ecb333f
ONKAR WAGH <onkar.s.wagh@gamil.com>
2025-10-20 19:08:17 +0530

    Initial commit

---
+++ src/main/java/com/example/brokerrmi/rmi_stock_exchange/RmiStockExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.brokerrmi.rmi_stock_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiStockExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmiStockExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brokerrmi/rmi_stock_exchange/RmiStockExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brokerrmi.rmi_stock_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmiStockExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
